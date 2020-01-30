e49052273424b0aa3bdaa87921f05a8b44863c31
Uros Aksentijevic <48104716+theaksaa@users.noreply.github.com>
2020-01-30 23:05:15 +0100

    Add files via upload

--- main/main.ino
+++ main/main.ino
@@ -1,15 +1,18 @@
 #include <Servo.h> 
 #define us_N 3
+#define default_angle 30
 
 // SENSORS
-int servoPin = 2;
+int servoPin = 3;
 Servo servo; 
 
-int trigPin[us_N] = {15, 6, 4}; // 0 - levo, 1 - srednje, 2 - desno
-int echoPin[us_N] = {14, 7, 5};
+int speakerPin = 2;
+
+int trigPin[us_N] = {13, 6, 4}; // 0 - levo, 1 - srednje, 2 - desno
+int echoPin[us_N] = {12, 7, 5};
 
 // OTHER VARIABLES
-int delayTime = 2;
+int delayTime = 20;
 
 int stepUp = 0;
 int stepDown = 0;
@@ -25,10 +28,25 @@ void setup()
         pinMode(trigPin[i], OUTPUT), pinMode(echoPin[i], INPUT);
 
     servo.attach(servoPin);
-    servo.write(30);
+    servo.write(default_angle);
+
+    beep(91, 5, 10, 100);
+
     Serial.println(calibration());
-    
 }
+
+//=======================================================================// SPEAKER
+
+void beep(int note, int n, int ntime, int dtime)
+{
+    for(int i = 0; i < n; i++)
+    {
+        tone(speakerPin, note, ntime * (1000 / note));
+        delay(dtime);
+    }
+}
+
+//=======================================================================// ANGLES
 int calculateMinimumAngle()
 {
   	int angle=45;
@@ -47,20 +65,25 @@ int calculateMinimumAngle()
 */
 int angleCalib(int k)
 {
+    beep(64, 1, 100, 1000);
     if(k == 0)
     {
         servo.write(k);
+        beep(13, 1, 10, 100);
         return getDistance(1);
     }
     
     servo.write(k);
     Serial.println("krece kalibracija (gledajte pravo u zid, imate 2 sekunde)");
+    beep(13, 2, 10, 100);
     delay(2000);
     int distance1 = getDistance(1);
     Serial.println("kalibracija (pomerite se jedan korak napred, imate 3 sekunde)");
+    beep(13, 3, 10, 100);
     delay(3000);
     int distance2 = getDistance(1);
     Serial.println("kalibracija zavrsena");
+    beep(13, 1, 10, 100);
 
     if(distance1 - distance2 <= 0) return -1;
     return distance1 - distance2;
@@ -69,11 +92,17 @@ int angleCalib(int k)
 int calibration()
 {
     int sDown = angleCalib(0); // CALIBRATION WHEN SERVO ANGLE IS 0 (down angle)
-    int sUp = angleCalib(30); // CALIBRATION WHEN SERVO ANGLE IS 30 (flat angle)
+    int sUp = angleCalib(default_angle); // CALIBRATION WHEN SERVO ANGLE IS 30 (flat angle)
+    sDown = sUp;
 
-    if(sUp == -1 || sDown == -1) return 0;
+    if(sUp <= -1 || sDown <= -1)
+    {
+        beep(17, 5, 10, 100);
+        return 0;
+    }
     stepUp = sUp;
     stepDown = sDown;
+    beep(81, 5, 10, 100);
     return 1;
 }
 
@@ -107,21 +136,21 @@ int checkObstacle(int angle)
     int distance = getDistance(1);
 
     if(angle <= 25 && distance <= stepUp) return 1;
-    if(angle <= 5 && distance <= stepDown) return 1;
+    if(angle <= 13 && distance <= stepDown) return 1;
     return 0;
 }
 
 void loop()
 {
-    for(int i = 30; i >= 0; i--)
+    for(int i = 35; i >= 0; i--)
     {
-        Serial.println(checkObstacle(i));
-        delayMicroseconds(delayTime); 
+        if(checkObstacle(i)) beep(57, 5, 10, 100);
+        delay(delayTime); 
     }
-    for(int i = 0; i < 30; i++)
+    for(int i = 0; i < 35; i++)
     {
-        Serial.println(checkObstacle(i));
-        delayMicroseconds(delayTime); 
+        if(checkObstacle(i)) beep(57, 5, 10, 100);
+        delay(delayTime); 
     }
     /*Serial.print("korak: ");
     Serial.println(calibration(stepUp, stepDown));
